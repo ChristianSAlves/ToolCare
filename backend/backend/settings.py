b35b4854860d8287f29bd544b10d4db11303a3fe
ChristianSAlves <christiansousaalves17@gmail.com>
2024-03-25 12:22:39 -0300

    Utilizando poetry ao inves de VENV

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=u9lhq02__-61^61c=2a2l^enlqvrtt-f=byn5edd0)+lbsglu'
+SECRET_KEY = 'django-insecure-udtit1tb7=qnzy(9kd3+e1h9l7ot8%uitm!^r5j8m%z55ix^ui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
