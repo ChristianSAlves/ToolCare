62fc7b7f56a62d10bc9c693192b9bb4f78a9bcb3
ChristianSAlves <christiansousaalves17@gmail.com>
2024-04-17 16:17:41 -0300

    adiçao de dependencias no poetry

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,11 +30,13 @@ INSTALLED_APPS = [
     'app_backend',
     'backend',
     'cpf_field',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -109,6 +111,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:8000",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
