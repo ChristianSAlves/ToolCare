d0ce7e8ab0ecc852aa55752a53d1a593e7ad97ab
Alex <alexcole1208@gmail.com>
2024-05-13 20:25:36 -0300

    card de teste para o christian testar no backend

---
+++ frontend/src/teste_card.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import { CardTeste } from './components/CardFerramentas/card_ferramentas.js'
+import React from 'react'
+
+
+export default class CardFerramentas extends React.Component {
+    
+
+    render() {
+        
+        return (
+            <CardTeste></CardTeste>
+            )
+    }
+}
