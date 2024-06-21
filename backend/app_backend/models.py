5fe9318cd87fe223ba44e3ecaaf2235f419a1076
ChristianSAlves <christiansousaalves17@gmail.com>
2024-06-21 14:41:26 -0300

    Desativaçao do funcionario com emprestimo autenticada

--- backend/app_backend/models.py
+++ backend/app_backend/models.py
@@ -7,18 +7,6 @@ import datetime
 def upload_path(instance, filename):
     return '/'.join(['funcionarios', str(instance.nome), filename])
 
-class Ferramenta(models.Model):
-    codFerramenta = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=255)
-    numSerie = models.CharField(max_length=20, unique=True)
-    descricao = models.TextField(null=True)
-    imgFerramenta = models.ImageField(upload_to='ferramentas/')
-    dataAquisicao = models.DateField()
-    status = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.nome
-
 class Setor(models.Model):
     codigoSetor = models.AutoField(primary_key=True)
     nomeSetor = models.CharField(max_length=255)
@@ -35,12 +23,57 @@ class Cargo(models.Model):
     def __str__(self):
         return self.nomeCargo
 
+class Ferramenta(models.Model):
+    codFerramenta = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    numSerie = models.CharField(max_length=20, unique=True)
+    descricao = models.TextField(null=True)
+    imgFerramenta = models.ImageField(upload_to='ferramentas/')
+    dataAquisicao = models.DateField()
+    status = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
+
+    def clean(self):
+        if self.status == 'Baixa' and self.status in ['Emprestada', 'Manutenção']:
+            raise ValidationError('Não é possível atualizar o status para Baixa se o status atual é Emprestada ou Manutenção')
+
+class Funcionario(models.Model):
+    idFuncionario = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=30)
+    matriculaFuncionario = models.CharField(max_length=50, unique=True)
+    cpf = CPFField(masked=True, unique=True)  # To enable auto-mask xxx.xxx.xxx-xx
+    codigoSetor = models.ForeignKey(Setor, on_delete=models.SET_NULL, null=True)
+    codigoCargo = models.ForeignKey(Cargo, on_delete=models.SET_NULL, null=True)
+    imgFunc = models.ImageField(null=True, blank=True, upload_to=upload_path)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+    
+    def get_absolute_url(self):
+        return reverse('FuncionarioView', kwargs={'matriculaFuncionario': self.matriculaFuncionario})
+
+    def clean(self):
+        if not self.status:
+            active_loans = Emprestimo.objects.filter(
+                matriculaFuncionario=self,
+                dataDevolucao__isnull=True
+            ).exists()
+            if active_loans:
+                raise ValidationError(
+                    "Não é possível desativar o funcionário enquanto houver empréstimos ativos."
+                )
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(Funcionario, self).save(*args, **kwargs)
+
 class Emprestimo(models.Model):
     codigoEmprestimo = models.AutoField(primary_key=True)
-    matriculaFuncionario = models.CharField(max_length=50)
-    numSerie = models.ForeignKey(
-        Ferramenta, on_delete=models.CASCADE, null=True
-    )
+    matriculaFuncionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
+    numSerie = models.ForeignKey(Ferramenta, on_delete=models.CASCADE, null=True)
     dataEmprestimo = models.DateField(default=datetime.date.today)
     dataDevolucao = models.DateField(null=True)
     observacoes = models.TextField(null=True)
@@ -54,42 +87,6 @@ class Emprestimo(models.Model):
             ferramenta = self.numSerie
             ferramenta.status = "Disponível"
             ferramenta.save()
-
-class Funcionario(models.Model):
-    idFuncionario = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=30)
-    matriculaFuncionario = models.CharField(max_length=50, unique=True)
-    cpf = CPFField(masked=True, unique=True)  # To enable auto-mask xxx.xxx.xxx-xx
-    codigoSetor = models.ForeignKey(
-        Setor, on_delete=models.SET_NULL, null=True
-    )
-    codigoCargo = models.ForeignKey(
-        Cargo, on_delete=models.SET_NULL, null=True
-    )
-    imgFunc = models.ImageField(null=True, blank=True, upload_to=upload_path)
-    status = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.nome
-    
-    def get_absolute_url(self):
-        return reverse('FuncionarioView', kwargs={'matriculaFuncionario': self.matriculaFuncionario})
-
-    def clean(self):
-        if not self.status:
-            active_loans = Emprestimo.objects.filter(
-                matriculaFuncionario=self.matriculaFuncionario,
-                dataDevolucao__isnull=True
-            ).exists()
-            if active_loans:
-                raise ValidationError(
-                    "Não é possível desativar o funcionário enquanto houver empréstimos ativos."
-                )
-
-    def save(self, *args, **kwargs):
-        self.clean()
-        super(Funcionario, self).save(*args, **kwargs)
-
 class ManutencaoFerramenta(models.Model):
     codigoManutencao = models.AutoField(primary_key=True)
     codFerramenta = models.ForeignKey(
